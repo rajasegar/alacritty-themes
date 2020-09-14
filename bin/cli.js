528f5bfe1ebf025d7cfeeef7730f54d65a66a3b5
Rajasegar <rajasegar.c@gmail.com>
2020-09-14 17:48:00 +0530

    BREAKING CHANGE: add more themes with search prompt

--- bin/cli.js
+++ bin/cli.js
@@ -4,17 +4,33 @@ const fs = require("fs");
 const path = require("path");
 const applyTheme = require("../index");
 const inquirer = require("inquirer");
+const fuzzy = require("fuzzy");
 
 const themesDir = path.join(__dirname, "..", "themes/");
 const themes = fs.readdirSync(themesDir).map((f) => f.replace(".yml", ""));
 const themePrompts = {
-  type: "list",
+  type: "autocomplete",
   name: "theme",
   message: "Select a theme:",
-  choices: themes,
+  source: searchThemes,
 };
 
+function searchThemes(answers, input) {
+  input = input || '';
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      var fuzzyResult = fuzzy.filter(input, themes);
+      resolve(
+        fuzzyResult.map(function (el) {
+          return el.original;
+        })
+      );
+    }, 100);
+  });
+}
+
 function main() {
+  inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
   inquirer.prompt(themePrompts).then((answers) => {
     const { theme } = answers;
     applyTheme(theme);
---
+++ convert.js
@@ -0,0 +1,74 @@
+'use strict';
+
+// this is the conversion script to 
+// convert the themes in terminal.sexy repo
+// from json to yml
+const yaml = require('yaml');
+
+const fs = require('fs');
+const path = require('path');
+
+console.log('Convert yml to js');
+
+const base16 = path.resolve('../terminal.sexy/dist/schemes/base16'); 
+const collection = path.resolve('../terminal.sexy/dist/schemes/collection'); 
+const xcolors = path.resolve('../terminal.sexy/dist/schemes/xcolors.net'); 
+const folders = [base16, collection, xcolors];
+//console.log(folder);
+
+folders.forEach(folder => {
+  fs.readdir(folder, (err,files) => {
+    if(err) throw err;
+    console.log(files);
+    files.forEach(f => {
+      const data = fs.readFileSync(`${folder}/${f}`, 'utf8');
+      const json = JSON.parse(data);
+      const newJson = {
+        colors: {
+          name: json.name,
+          author: json.author,
+          primary: {
+            background: json.background,
+            foreground: json.foreground,
+          },
+          cursor: {
+            text: json.background,
+            cursor: json.foreground,
+          },
+          normal: {
+            black: json.color[0],
+            red: json.color[1],
+            green: json.color[2],
+            yellow: json.color[3],
+            blue: json.color[4],
+            magenta: json.color[5],
+            cyan: json.color[6],
+            white: json.color[7],
+          },
+          bright: {
+            black: json.color[8],
+            red: json.color[9],
+            green: json.color[10],
+            yellow: json.color[11],
+            blue: json.color[12],
+            magenta: json.color[13],
+            cyan: json.color[14],
+            white: json.color[15],
+          }
+
+        }
+      };
+      const output = yaml.stringify(newJson);
+      const _baseName = path.basename(f, '.json');
+      const fileName = _baseName[0].toUpperCase() + _baseName.slice(1) + '.yml';
+      const ymlFile = 'themes/' + fileName;
+      if(!fs.existsSync(ymlFile)) {
+        fs.writeFile(ymlFile, output, (err, data) => {
+          if(err) throw err;
+          console.log(`${ymlFile} written successfully.`);
+        });
+      }
+    });
+
+  });
+});
