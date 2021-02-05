97ef24487131f794715cb309c8077377828705e2
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-02-05 13:22:53 +0530

    chore: clean restore config with temp files
    1. setup eslint and prettier

--- bin/cli.js
+++ bin/cli.js
@@ -1,43 +1,62 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 const prompts = require('prompts');
+const temp = require('temp').track();
 
-const { applyTheme, getPrevTheme,restoreColors  } = require("../index");
+const { applyTheme, createConfigFile, ymlPath } = require('../index');
 
-const themesDir = path.join(__dirname, "..", "themes/");
-const themes = fs.readdirSync(themesDir).map((f) => f.replace(".yml", ""));
-
-
+const themesDir = path.join(__dirname, '..', 'themes/');
+const themes = fs.readdirSync(themesDir).map((f) => f.replace('.yml', ''));
 
 function main() {
-  const oldColors  = getPrevTheme();
-
-  (async () => {
-    const response = await prompts({
-      type: 'autocomplete',
-      name: 'theme',
-      message: 'Select a theme',
-      choices: themes.map(t => {
-        return {
-          title: t,
-          value: t
-        };
-      }),
-      onState: (state) => {
-        applyTheme(state.value, true); // set preview true
-      }
-    });
-
-    if(response.theme) {
-      applyTheme(response.theme);
+  if (process.argv.length > 2) {
+    if (process.argv.includes('--create') || process.argv.includes('-c')) {
+      createConfigFile();
     } else {
-      // User cancelled the selection, so restore to old colors
-      restoreColors(oldColors);
+      // the 3rd arg is theme name
+      applyTheme(process.argv[2]);
     }
+  } else {
+    // Copy original config to new file
+    //
+    const tempDir = temp.mkdirSync('alacritty-themes');
+    const backupPath = path.join(tempDir, 'alacritty.yml');
+    /* eslint-disable */
+    fs.copyFile(ymlPath, backupPath, (err) => {
+      if (err) throw err;
+    });
+    /* eslint-enable */
 
-  })();
+    (async () => {
+      const response = await prompts({
+        type: 'autocomplete',
+        name: 'theme',
+        message: 'Select a theme',
+        choices: themes.map((t) => {
+          return {
+            title: t,
+            value: t,
+          };
+        }),
+        onState: (state) => {
+          applyTheme(state.value, true); // set preview true
+        },
+      });
+
+      if (response.theme) {
+        applyTheme(response.theme);
+      } else {
+        // Restore original config
+        fs.readFile(backupPath, 'utf8', (err, data) => {
+          fs.writeFile(ymlPath, data, 'utf8', (err2) => {
+            if (err2) throw err2;
+          });
+        });
+      }
+    })();
+  }
 }
 
 main();
--- index.js
+++ index.js
@@ -1,154 +1,76 @@
-const YAML = require("yaml");
-const fs = require("fs");
-const path = require("path");
-const prompts = require("prompts");
-const { Pair } = require("yaml/types");
+const YAML = require('yaml');
+const fs = require('fs');
+const path = require('path');
+const { Pair } = require('yaml/types');
 
 // alacritty.yml path
-const ymlPath = (process.env.OS === "Windows_NT")
-  ? path.resolve(
-    process.env.APPDATA,
-    "alacritty/alacritty.yml"
-  )
-  : path.resolve(
-    process.env.HOME,
-    ".config/alacritty/alacritty.yml"
-  );
-
+const ymlPath =
+  process.env.OS === 'Windows_NT'
+    ? path.resolve(process.env.APPDATA, 'alacritty/alacritty.yml')
+    : path.resolve(process.env.HOME, '.config/alacritty/alacritty.yml');
 
 function createConfigFile() {
+  const templatePath = path.join(__dirname, 'alacritty.yml');
+  const configTemplate = fs.readFileSync(templatePath, 'utf8');
 
-  const createConfigPrompt = {
-    type: "confirm",
-    name: "createConfig",
-    message:
-    "Looks like you don't have alacritty config file. Do you want to create one?",
-  };
+  const homeDir =
+    process.env.OS === 'Windows_NT'
+      ? path.join(process.env.APPDATA, 'alacritty/')
+      : path.join(process.env.HOME, '.config/alacritty/');
 
-  (async () => {
-
-    const answers = await prompts(createConfigPrompt);
-
-    if (answers.createConfig) {
-      const templatePath = path.join(__dirname, "alacritty.yml");
-      const configTemplate = fs.readFileSync(templatePath, "utf8");
-
-      const homeDir = (process.env.OS === "Windows_NT")
-        ? path.join(process.env.APPDATA, "alacritty/")
-        : path.join(process.env.HOME, ".config/alacritty/");
-
-      // If .config/alacritty folder doesn't exists, create one
-      if (!fs.existsSync(homeDir)) {
-        fs.mkdirSync(homeDir);
-      }
-
-      fs.writeFileSync(ymlPath, configTemplate, "utf8");
-      console.log("New config file created.");
-    }
-
-  })();
-}
-
-function getPrevTheme() {
-  let colors;
-  if(!fs.existsSync(ymlPath)) {
-    console.log('file not exists');
-    createConfigFile();
-
-    const themePath = path.join(__dirname, `themes/Dracula.yml`);
-    const themeFile = fs.readFileSync(themePath, "utf8");
-
-    const themeDoc = YAML.parseDocument(themeFile);
-
-    // Find the colors key in theme.yml
-    colors  = themeDoc.contents.items.filter(
-      (i) => i.key.value === "colors"
-    )[0];
-  } else {
-    console.log('file exists');
-
-    const data = fs.readFileSync(ymlPath, "utf8");
-
-    const doc = YAML.parseDocument(data);
-
-    // Find the colors key in user's alacritty.yml
-    colors = doc.contents.items.filter((i) => i.key.value === "colors")[0];
-
-
+  // If .config/alacritty folder doesn't exists, create one
+  if (!fs.existsSync(homeDir)) {
+    fs.mkdirSync(homeDir);
   }
 
-  return colors;
-
+  fs.writeFileSync(ymlPath, configTemplate, 'utf8');
+  console.log('New config file created.');
 }
 
 function updateTheme(data, theme, preview = false) {
-  let prevColors;
   const themePath = path.join(__dirname, `themes/${theme}.yml`);
-  const themeFile = fs.readFileSync(themePath, "utf8");
+  const themeFile = fs.readFileSync(themePath, 'utf8');
 
   const doc = YAML.parseDocument(data);
 
   const themeDoc = YAML.parseDocument(themeFile);
 
   // Find the colors key in user's alacritty.yml
-  const colors = doc.contents.items.filter((i) => i.key.value === "colors")[0];
+  const colors = doc.contents.items.filter((i) => i.key.value === 'colors')[0];
 
   // Find the colors key in theme.yml
   const themeColors = themeDoc.contents.items.filter(
-    (i) => i.key.value === "colors"
+    (i) => i.key.value === 'colors'
   )[0];
 
   // colors key is commented out or not available
   if (!colors) {
     // Create new colors key and assign value from theme
-    doc.contents.items.push(new Pair("colors", themeColors.value));
+    doc.contents.items.push(new Pair('colors', themeColors.value));
   } else {
-
-    // Save previous colors
-    prevColors = colors.value;
     // Update colors key
     colors.value = themeColors.value;
   }
 
   const newContent = String(doc);
 
-  fs.writeFile(ymlPath, newContent, "utf8", (err) => {
+  fs.writeFile(ymlPath, newContent, 'utf8', (err) => {
     if (err) throw err;
-    if(!preview) {
+
+    if (!preview) {
       console.log(`The theme ${theme} has been applied successfully!`);
     }
   });
 }
 
-function restoreColors(oldColors) {
-
-  fs.readFile(ymlPath, "utf8", (err, data) => {
-    const doc = YAML.parseDocument(data);
-
-    // Find the colors key in user's alacritty.yml
-    const colors = doc.contents.items.filter((i) => i.key.value === "colors")[0];
-
-    // Restore colors value
-    colors.value = oldColors.value;
-
-    const newContent = String(doc);
-
-    fs.writeFile(ymlPath, newContent, "utf8", (err) => {
-      if (err) throw err;
-    });
+function applyTheme(theme, preview = false) {
+  fs.readFile(ymlPath, 'utf8', (err, data) => {
+    updateTheme(data, theme, preview);
   });
 }
 
-function applyTheme(theme, preview = false) {
-  fs.readFile(ymlPath, "utf8", (err, data) => {
-    updateTheme(data, theme, preview);
-  });
-};
-
 module.exports = {
   applyTheme,
-  getPrevTheme,
-  restoreColors,
   ymlPath,
-  createConfigFile
+  createConfigFile,
 };
