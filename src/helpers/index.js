97ba2e97b02d60635d943c7199b63770f3bf13d9
Juan Vasquez <juan@logicalbricks.com>
2021-08-31 08:34:22 -0500

    Improvement: find existing alacritty file config

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,5 +1,14 @@
 const possibleLocations = require('./possibleLocations');
+const {
+  alacrittyFileExists,
+  alacrittyConfigPath,
+} = require('./existingAlacrittyFile');
+
+const { NoAlacrittyFileFoundError } = require('./alacrittyThemesErrors');
 
 module.exports = {
   possibleLocations,
+  alacrittyFileExists,
+  alacrittyConfigPath,
+  NoAlacrittyFileFoundError,
 };
